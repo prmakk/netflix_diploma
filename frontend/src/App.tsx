54c6830135c0938fbf766b1c1ee218873496dd58
prmk <priymakvlad420@gmail.com>
2024-10-21 17:50:23 +0300

    chore: init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import "./styles/normalize.css";
+import "./styles/global.scss";
+
+function App() {
+    return <>it is working</>;
+}
+
+export default App;
---
+++ frontend/src/types/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="vite/client" />
+
+declare module "*.svg";
+declare module "*.scss";
+declare module "*.gif";
