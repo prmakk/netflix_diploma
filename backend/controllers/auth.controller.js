586fad5027356592d7dc236761ff6e151dcfcdad
prmk <priymakvlad420@gmail.com>
2024-10-22 12:28:52 +0300

    feat: added server routes and auth controllers

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export async function signup(req, res) {
+    res.json("Signup route");
+}
+
+export async function login(req, res) {
+    res.json("Login route");
+}
+
+export async function logout(req, res) {
+    res.json("Logout route");
+}
