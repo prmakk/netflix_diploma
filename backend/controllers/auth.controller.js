bbca81fd65b036f1c5007420e8ea378eda8ee9ba
prmk <priymakvlad420@gmail.com>
2024-10-25 14:15:39 +0300

    feat: implemented logout logic

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -78,5 +78,17 @@ export async function login(req, res) {
 }
 
 export async function logout(req, res) {
-    res.json("Logout route");
+    try {
+        res.clearCookie("jwt-netflix");
+        res.status(200).json({
+            success: true,
+            message: "Logged out successfully",
+        });
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        });
+    }
 }
